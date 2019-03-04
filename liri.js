163558cc00a409897a86a56e3c3e7da0ed681bb0
MetalJester <acjswanson@gmail.com>
2019-03-03 20:15:43 -0800

    added spotify function and switch

--- liri.js
+++ liri.js
@@ -18,7 +18,6 @@ require('dotenv').config();
 
 //Global variables
 var keys = require("./keys.js");
-// var spotify = new Spotify(keys.spotify);
 var axios = require("axios");
 var liriCommand = process.argv[2];
 var userChoice = process.argv.slice(3).join(" ");
@@ -26,34 +25,53 @@ var userChoice = process.argv.slice(3).join(" ");
 //switches for various commands
 switch (liriCommand) {
     case "movie-this":
-    movieThis();
-    break;
+        movieThis();
+        break;
 
-    // case "spotify-this-song":
-    // spotifyThis();
-    // break;
+    case "spotify-this-song":
+        spotifyThis();
+        break;
 }
 
 // spotify-this-song function
-function spotifyThis () {
+function spotifyThis() {
+
+    var Spotify = require('node-spotify-api');
+    var spotify = new Spotify(keys.spotify);
 
     if (!userChoice) {
         userChoice = "The Sign";
     }
 
+    spotify
+        .search({ type: 'track', query: userChoice })
+        .then(function (data) {
+            var songInfo = data.tracks.items;
+            for (var i = 0; i < 1; i++){
+                console.log(
+                    `Artist(s): ${songInfo[i].artists[0].name}\n`,
+                    `Song Title: ${songInfo[i].name}\n`,
+                    `Preview spotify link: ${songInfo[i].preview_url}\n`,
+                    `Album Title: ${songInfo[i].album.name}\n`)}
+        })
+        .catch(function (err) {
+            console.log(err);
+        });
+
 }
 
 
+
 //movie-this function for the OMDB API;
-function movieThis () {
-    
+function movieThis() {
+
     // if the user doesn't add a movie name, then Mr. Nobody will be searched for as default
     if (!userChoice) {
         userChoice = "Mr. Nobody";
     }
 
-    var movieQueryURL = "http://www.omdbapi.com/?t=" + userChoice + "&y=&plot=short&apikey=trilogy";    
-       
+    var movieQueryURL = "http://www.omdbapi.com/?t=" + userChoice + "&y=&plot=short&apikey=trilogy";
+
     axios.get(movieQueryURL).then(
         function (response) {
             console.log(
@@ -65,8 +83,8 @@ function movieThis () {
                 `Language of the movie: ${response.data.Language}\n`,
                 `Plot of the movie: ${response.data.Plot}\n`,
                 `Actors in the movie: ${response.data.Actors}\n`)
-    
-      }
+
+        }
     );
 }
 
